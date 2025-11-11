95f8115aa1895c3a82cd6b49cd91eb082fc288a0
Lala <Addixt@gmail.com>
2025-11-11 16:17:08 +0900

    IntroPage 수정
    Page구조 정리,
    
    IntroModel -> 편집 전용 페이지
    IntroPageViewController -> 구현

--- iOS5team2MainboardUiKit/iOS5team2MainboardUiKit/Intro/IntroModel.swift
+++ iOS5team2MainboardUiKit/iOS5team2MainboardUiKit/Intro/IntroModel.swift
@@ -8,15 +8,38 @@
 import UIKit
 
 struct IntroModel {
-    let icon: String
-    let title: String
-    let description: String
-    let backgroundColor: UIColor
+    struct IntPage {
+        let icon: String
+        let title: String
+        let description: String
+        let backgroundColor: UIColor
+    }
 
-    static let pages: [IntroModel] = [
-        .init(icon: "swift", title: "제목1", description: "테스트", backgroundColor: .systemMint),
-        .init(icon: "star", title: "제목2", description: "테스트", backgroundColor: .systemYellow),
-        .init(icon: "lock", title: "제목3", description: "테스트", backgroundColor: .systemTeal),
-        .init(icon: "bolt", title: "제목4", description: "테스트", backgroundColor: .systemIndigo)
+    static let introSeenKey = "hasSeenIntro"
+    
+// 인트로 각 페이지에 들어갈 아이콘, 제목, 문구
+    static let pages: [IntPage] = [
+        .init(icon: "swift",
+              title: "페이지1",
+              description: "테스트1",
+              backgroundColor: .systemMint),
+        
+        .init(icon: "star",
+              title: "페이지2",
+              description: "테스트2",
+              backgroundColor: .systemYellow),
+        
+        .init(icon: "lock",
+              title: "페이지3",
+              description: "테스트3",
+              backgroundColor: .systemTeal),
+        
+        .init(icon: "bolt",
+              title: "페이지4",
+              description: "테스트4",
+              backgroundColor: .systemIndigo)
     ]
+    
+// 취향 선택에 사용할 언어목록
+    static let languages: ["Swift", "Kotlin", "Python", "JavaScript", "C++", "테스트", "안녕하세요", "반갑습니다", "안깨지나", "가나다라", "마바사아"]
 }
