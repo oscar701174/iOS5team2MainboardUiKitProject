192fac83137a2a8014380b13aa048bbb3d25ec68
Lala <Addixt@gmail.com>
2025-11-12 08:02:27 +0900

    Intro
    1. Added icons
    2. Further enhancements
       - Customizable pages / languages

--- iOS5team2MainboardUiKit/iOS5team2MainboardUiKit/Intro/IntroModel.swift
+++ iOS5team2MainboardUiKit/iOS5team2MainboardUiKit/Intro/IntroModel.swift
@@ -12,30 +12,75 @@ struct IntroModel {
         let icon: String
         let title: String
         let description: String
+
         let backgroundColor: UIColor
+        let titleFont: UIFont
+        let descriptionFont: UIFont
+        let fontColor: UIColor
+
+        init(
+            icon: String,
+            title: String,
+            description: String,
+            backgroundColor: UIColor = .systemBackground,
+            titleFont: UIFont = .systemFont(ofSize: 28, weight: .bold),
+            descriptionFont: UIFont = .systemFont(ofSize: 16, weight: .medium),
+            fontColor: UIColor = .label
+        ) {
+            self.icon = icon
+            self.title = title
+            self.description = description
+            self.backgroundColor = backgroundColor
+            self.titleFont = titleFont
+            self.descriptionFont = descriptionFont
+            self.fontColor = fontColor
+        }
     }
 
-    static let introSeenKey = "hasSeenIntro"
-// 인트로 각 페이지에 들어갈 아이콘, 제목, 문구
     static let pages: [IntPage] = [
-        .init(icon: "swift",
-              title: "페이지1",
-              description: "테스트1",
-              backgroundColor: .systemMint),
-        .init(icon: "star",
-              title: "페이지2",
-              description: "테스트2",
-              backgroundColor: .systemYellow),
-        .init(icon: "lock",
-              title: "페이지3",
-              description: "테스트3",
-              backgroundColor: .systemTeal),
-        .init(icon: "bolt",
-              title: "페이지4",
-              description: "테스트4",
-              backgroundColor: .systemIndigo)
+        .init(
+            icon: "swift",
+            title: "페이지1",
+            description: "테스트1 학교종이 땡땡땡",
+            backgroundColor: .systemMint,
+//            titleFont: .systemFont(ofSize: 28, weight: .heavy),
+//            descriptionFont: .systemFont(ofSize: 16, weight: .medium),
+            fontColor: .white
+        ),
+        .init(
+            icon: "star",
+            title: "페이지2",
+            description: "테스트2 어서모이자",
+            backgroundColor: .systemYellow,
+//            titleFont: .systemFont(ofSize: 28, weight: .heavy),
+//            descriptionFont: .systemFont(ofSize: 16, weight: .medium),
+            fontColor: .black
+        ),
+        .init(
+            icon: "lock",
+            title: "페이지3",
+            description: "테스트3 선생님이 우리를",
+            backgroundColor: .systemTeal,
+//            titleFont: .systemFont(ofSize: 28, weight: .heavy),
+//            descriptionFont: .systemFont(ofSize: 16, weight: .medium),
+            fontColor: .white
+        ),
+        .init(
+            icon: "bolt",
+            title: "페이지4",
+            description: "테스트4 기다리신다",
+            backgroundColor: .systemIndigo,
+//            titleFont: .systemFont(ofSize: 28, weight: .heavy),
+//            descriptionFont: .systemFont(ofSize: 16, weight: .medium),
+            fontColor: .white
+        )
     ]
-// 취향 선택에 사용할 언어목록
-    static let languages: [String] = ["Swift", "Kotlin", "Python", "JavaScript", "C++",
-                                      "테스트", "안녕하세요", "반갑습니다"]
+
+    static let languages: [String] = [
+        "Angular", "C", "Django", "Docker", "Java",
+        "JavaScript", "Kotlin", "Kubernetes", "NoSQL", "PHP",
+        "Python", "React", "Spring", "Swift", "Vuejs"
+    ]
+
+    static let introSeenKey = "hasSeenIntro"
 }
