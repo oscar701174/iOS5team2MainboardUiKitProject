da0b381f11d64133212c68004da91ff942a0a3c8
Oscar <oscar701174@gmail.com>
2025-11-12 14:21:40 +0900

    merged with origin/main

--- iOS5team2MainboardUiKit/iOS5team2MainboardUiKit/ClipLibrary/MyClipViewController.swift
+++ iOS5team2MainboardUiKit/iOS5team2MainboardUiKit/ClipLibrary/MyClipViewController.swift
@@ -3,14 +3,6 @@ import UIKit
 
 class MyClipViewController: UIViewController {
 
-    /*
-    // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
